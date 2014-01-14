20ee00f26fe029ca5ec0ea547a99b879c3d84274
Rick Barber <barber5@stanford.edu>
2014-01-14 09:29:00 -0800

    sql

--- dbcreate.sql
+++ dbcreate.sql
@@ -42,7 +42,7 @@ SET default_with_oids = false;
 
 DROP database if exists ethea;
 
-CREATE DATABASE ethea
+CREATE DATABASE feedEater
   WITH OWNER = postgres
        ENCODING = 'UTF8'
        TABLESPACE = pg_default
@@ -50,7 +50,7 @@ CREATE DATABASE ethea
        LC_CTYPE = 'en_US.UTF-8'
        CONNECTION LIMIT = -1       
        TEMPLATE template0;
-\connect ethea
+\connect feedEater
 CREATE EXTENSION pg_trgm;
 
 --
@@ -58,277 +58,45 @@ CREATE EXTENSION pg_trgm;
 -- Name: communities; Type: TABLE; Schema: public; Owner: postgres; Tablespace: 
 --
 
-CREATE TABLE communities (
+CREATE TABLE feeds (
     id uuid NOT NULL,
-    name character varying(255),
-    description character varying(2048),
+    name character varying(255) NOT NULL,
+    feed_url character varying(255) NOT NULL,
+    blog_url character varying(255),
+    extraction_rule character varying(1024),
+    pagination_rule character varying(1024),
     created timestamp without time zone,
-    updated timestamp without time zone,
-    image character varying(255)
+    updated timestamp without time zone,    
 );
 
 
-ALTER TABLE public.communities OWNER TO postgres;
-
---
--- TOC entry 169 (class 1259 OID 17056)
--- Name: communities_itemtypes; Type: TABLE; Schema: public; Owner: postgres; Tablespace: 
---
-
-CREATE TABLE communities_itemtypes (
-    community_id uuid NOT NULL,
-    itemtype_id uuid NOT NULL
-);
-
-
-ALTER TABLE public.communities_itemtypes OWNER TO postgres;
-
---
--- TOC entry 170 (class 1259 OID 17059)
--- Name: communities_profiles; Type: TABLE; Schema: public; Owner: postgres; Tablespace: 
---
-
-CREATE TABLE communities_profiles (
-    community_id uuid NOT NULL,
-    profile_id uuid NOT NULL,
-    flag character varying(255),    
-    created timestamp without time zone,
-    updated timestamp without time zone
-);
-
-
-ALTER TABLE public.communities_profiles OWNER TO postgres;
-
---
--- TOC entry 171 (class 1259 OID 17062)
--- Name: itemlists; Type: TABLE; Schema: public; Owner: postgres; Tablespace: 
---
-
-CREATE TABLE itemlists (
-    id uuid NOT NULL,
-    name character varying(255),
-    profile_id uuid,
-    created timestamp without time zone,
-    updated timestamp without time zone
-);
-
-
-ALTER TABLE public.itemlists OWNER TO postgres;
-
---
--- TOC entry 183 (class 1259 OID 17472)
--- Name: itemlists_items; Type: TABLE; Schema: public; Owner: postgres; Tablespace: 
---
-
-CREATE TABLE itemlists_items (
-    item_id uuid NOT NULL,
-    itemlist_id uuid NOT NULL
-);
-
-
-ALTER TABLE public.itemlists_items OWNER TO postgres;
-
---
--- TOC entry 172 (class 1259 OID 17068)
--- Name: items; Type: TABLE; Schema: public; Owner: postgres; Tablespace: 
---
-
-CREATE TABLE items (
-    id uuid NOT NULL,
-    name character varying(255),
-    image character varying(255),
-    itemtype_id uuid,
-    description character varying(2048),
-    flag character varying(255),
-    created timestamp without time zone,
-    updated timestamp without time zone
-);
-
-
-ALTER TABLE public.items OWNER TO postgres;
-
---
--- TOC entry 173 (class 1259 OID 17074)
--- Name: itemtype; Type: TABLE; Schema: public; Owner: postgres; Tablespace: 
---
-
-CREATE TABLE itemtypes (
-    id uuid NOT NULL,
-    image character varying(255),
-    name character varying(255),
-    description character varying(2048),
-    created timestamp without time zone,
-    updated timestamp without time zone
-);
-
-
-ALTER TABLE public.itemtypes OWNER TO postgres;
-
---
--- TOC entry 174 (class 1259 OID 17080)
--- Name: messages; Type: TABLE; Schema: public; Owner: postgres; Tablespace: 
---
-
-CREATE TABLE messages (
-    id uuid NOT NULL,
-    from_id uuid,
-    to_id uuid,
-    content character varying(1024),
-    created timestamp without time zone,
-    updated timestamp without time zone
-);
-
-
-ALTER TABLE public.messages OWNER TO postgres;
-
---
--- TOC entry 175 (class 1259 OID 17086)
--- Name: posts; Type: TABLE; Schema: public; Owner: postgres; Tablespace: 
---
+ALTER TABLE public.feeds OWNER TO postgres;
 
 CREATE TABLE posts (
     id uuid NOT NULL,
-    title character varying(255),
-    content character varying(2048),
-    profile_id uuid,
-    community_id uuid,
-    likes integer,
-    parent_id uuid,
-    flag character varying(255),
-    root_id uuid,
-    deleted boolean DEFAULT FALSE,
+    title character varying(255) NOT NULL,
+    feed_id uuid NOT NULL,
+    byline character varying(1024),
+    post_date timestamp without time zone,
+    comment_feed character varying(255),
+    post_url character varying(255),
+    content text,    
     created timestamp without time zone,
-    updated timestamp without time zone
+    updated timestamp without time zone,    
 );
 
 
 ALTER TABLE public.posts OWNER TO postgres;
 
---
--- TOC entry 176 (class 1259 OID 17092)
--- Name: posts_items; Type: TABLE; Schema: public; Owner: postgres; Tablespace: 
---
 
-CREATE TABLE posts_items (
-    structure_id uuid NOT NULL,
-    item_id uuid NOT NULL
-);
+ALTER TABLE ONLY posts
+    ADD CONSTRAINT posts_pkey PRIMARY KEY (id);
 
+ALTER TABLE ONLY posts
+    ADD CONSTRAINT fk_posts FOREIGN KEY (feed_id) REFERENCES feeds(id) ON DELETE CASCADE;
 
-ALTER TABLE public.posts_items OWNER TO postgres;
-
-CREATE TABLE ethean_groups (
-    ethean_id uuid NOT NULL,
-    profile_id uuid,
-    other_id uuid,
-    group_name character varying(255) NOT NULL
-);
-
-ALTER TABLE public.ethean_groups OWNER TO postgres;
-
---
--- TOC entry 177 (class 1259 OID 17095)
--- Name: posts_tags; Type: TABLE; Schema: public; Owner: postgres; Tablespace: 
---
-
-CREATE TABLE posts_tags (
-    post_id uuid NOT NULL,
-    tag_id uuid NOT NULL
-);
-
-
-ALTER TABLE public.posts_tags OWNER TO postgres;
-
---
--- TOC entry 178 (class 1259 OID 17098)
--- Name: profiles; Type: TABLE; Schema: public; Owner: postgres; Tablespace: 
---
-
-CREATE TABLE profiles (
-    id uuid NOT NULL,
-    name character varying(255),
-    ethean_id uuid,
-    image character varying(255),
-    description character varying(2048),
-    created timestamp without time zone,
-    updated timestamp without time zone
-);
-
-
-ALTER TABLE public.profiles OWNER TO postgres;
-
---
--- TOC entry 179 (class 1259 OID 17104)
--- Name: structures; Type: TABLE; Schema: public; Owner: postgres; Tablespace: 
---
-
-CREATE TABLE structures (
-    id uuid NOT NULL,
-    content character varying(4048),
-    post_id uuid,
-    structure_type character varying(255),
-    created timestamp without time zone,
-    updated timestamp without time zone,
-    insert_pos integer
-);
-
-
-ALTER TABLE public.structures OWNER TO postgres;
-
---
--- TOC entry 180 (class 1259 OID 17110)
--- Name: suggestion; Type: TABLE; Schema: public; Owner: postgres; Tablespace: 
---
-
-CREATE TABLE suggestion (
-    id uuid NOT NULL,
-    title character varying(255),
-    content character varying(1024),
-    profile_id uuid,
-    item_id uuid,
-    created timestamp without time zone,
-    updated timestamp without time zone
-);
-
-
-ALTER TABLE public.suggestion OWNER TO postgres;
-
---
--- TOC entry 181 (class 1259 OID 17116)
--- Name: tags; Type: TABLE; Schema: public; Owner: postgres; Tablespace: 
---
-
-CREATE TABLE tags (
-    id uuid NOT NULL,
-    name character varying(255),
-    created timestamp without time zone,
-    updated timestamp without time zone
-);
-
-
-ALTER TABLE public.tags OWNER TO postgres;
-
---
--- TOC entry 182 (class 1259 OID 17119)
--- Name: etheans; Type: TABLE; Schema: public; Owner: postgres; Tablespace: 
---
-
-CREATE TABLE etheans (
-    name character varying(255),
-    email character varying(255),
-    id uuid NOT NULL,
-    hashed_pwd character varying(512),
-    access_token character varying(512),
-    email_token character varying(512),
-    flag character varying(255),
-    created timestamp without time zone,
-    updated timestamp without time zone,
-    salt character varying(25),
-    pwd_tk character varying(40)
-);
-
-
-ALTER TABLE public.etheans OWNER TO postgres;
+ALTER TABLE ONLY feeds
+    ADD CONSTRAINT feeds_pkey PRIMARY KEY (id);
 
 --
 -- TOC entry 2946 (class 0 OID 0)
